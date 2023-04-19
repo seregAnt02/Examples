23b221dc886997366c9dc8c385c6a83a7c98f982
s_antonov02 <s_antonov02@rambler.ru>
2023-04-19 13:23:49 +0500

    Создание папки git

---
+++ Example002_НelloUser/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите Ваше имя ");
+        string username = Console.ReadLine();
+        Console.WriteLine("Привет, ");
+        Console.WriteLine(username);
+    }
+}
